38bd6d27b6e7ce30312818f9f6b84109a1ed6ffe
Hameema <hameema@gmail.com>
2026-06-03 21:22:06 +0530

    gallery update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,7 +11,9 @@ import img7 from "../assets/images/WhatsApp Image 2026-05-22 at 9.52.31 PM.jpeg"
 import img8 from "../assets/images/WhatsApp Image 2026-05-22 at 9.52.31 PM (1).jpeg";
 
 import img9 from "../assets/images/WhatsApp Image 2026-05-22 at 9.52.32 PM.jpeg";
-import img10 from "../assets/images/WhatsApp Image 2026-05-22 at 9.52.28 PM.jpeg";
+
+import img11 from "../assets/images/WhatsApp Image 2026-06-03 at 9.05.07 PM.jpeg";
+import img12 from "../assets/images/WhatsApp Image 2026-06-03 at 6.53.46 PM.jpeg";
 
 function Gallery() {
   return (
@@ -28,26 +30,22 @@ function Gallery() {
           by our little stars every single day.
         </p>
 
-        {/* GALLERY GRID */}
-
         <div className="gallery-grid">
 
+          {/* Nursery Building */}
+          <img src={img11} alt="ABC Nursery Building Night View" />
+          <img src={img12} alt="ABC Nursery Building Day View" />
+
+          {/* Activities */}
           <img src={img1} alt="Kids Activity" />
           <img src={img2} alt="Classroom Learning" />
           <img src={img3} alt="Creative Play" />
           <img src={img4} alt="Fun Learning" />
-
           <img src={img5} alt="School Event" />
           <img src={img6} alt="Happy Students" />
           <img src={img7} alt="Celebration" />
           <img src={img8} alt="Group Activity" />
-
-          {/* LAST ROW */}
-
-          <div className="gallery-last-row">
-            <img src={img9} alt="Kids Event" />
-            <img src={img10} alt="Nursery Moments" />
-          </div>
+          <img src={img9} alt="Kids Event" />
 
         </div>
 
